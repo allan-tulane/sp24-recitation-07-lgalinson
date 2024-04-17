67c61cbd9a359624ed76a74aa224fe33555b523f
Lakeland Galinson <lakeland@galinson.com>
2024-04-17 17:19:26 -0500

    Final

--- answers.md
+++ answers.md
@@ -1,6 +1,6 @@
 # CMPS 2200 Recitation 10## Answers
 
-**Name:**_________________________
+**Name:** Lakeland Galinson
 **Name:**_________________________
 
 
@@ -9,9 +9,15 @@ Place all written answers from `recitation-07.md` here for easier grading.
 
 
 - **2)**
+The work is $W(2m) + W(n) + O(1)$
 
 - **4)**
+In the worst case, we select a starting node and call reachable once. This function traverses the entire graph. If it reaches every node, the graph is connected. Reachable is called only once in all cases.
 
 - **5)**
+The work is $W(2m) + W(n) +O(1)$
 
 - **7)**
+Switching from an adjacency list to an adjacency matrix changes the `reachable` function's complexity from $O(n + m)$ to $O(n^2)$ because you must check all potential connections for each node, making it less efficient for sparse graphs.
+
+
--- main.py
+++ main.py
@@ -20,23 +20,53 @@ def reachable(graph, start_node):
         ###TODO
         pass
     return result
+def reachable(graph, start_node):
+    """
+    Returns the set of nodes reachable from start_node
+    """
+    
+    result = set([start_node])
+    frontier = set([start_node])
 
+    while len(frontier) != 0:
+        current_node = frontier.pop()  # remove and get an element from the frontier
+        for neighbor in graph[current_node]:
+            if neighbor not in result:
+                frontier.add(neighbor)
+                result.add(neighbor)
 
-
+    return result
 
 
 def connected(graph):
-    ### TODO
-    pass
+    if not graph:
+        return True  # An empty graph is connected
+    
+    # Start from any node; here we take the first node in the graph dictionary
+    start_node = next(iter(graph))
+    
+    # Use the reachable function to get all nodes reachable from the start node
+    reachable_nodes = reachable(graph, start_node)
+    
+    # The graph is connected if the number of reachable nodes is equal to the number of nodes in the graph
+    return len(reachable_nodes) == len(graph)
 
 
 
 
 def n_components(graph):
     """
-    Returns:
-      the number of connected components in an undirected graph
+    Returns the number of connected components in an undirected graph
     """
-    ### TODO
-    pass
+    visited = set()
+    component_count = 0
+    
+    for node in graph:
+        if node not in visited:
+            # Node not visited, so it's a start of a new component
+            reachable_nodes = reachable(graph, node)
+            visited.update(reachable_nodes)
+            component_count += 1
+            
+    return component_count
 
